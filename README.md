971ac2b3fb64d7c51b0412ca4c8534fcba45557c
Edward Santos <edwardsantos@Egret.local>
2018-04-30 15:33:24 +0800

    4th

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # Basic-API
-# Basic-API
+# How to Use
+
+This project is not linked to an actual database just yet. As such, testing of this API project
+requires the use of Postman.
+
+### Testing GET
+Can only be used at
+```sh
+http://localhost:8080/list/
+http://localhost:8080/list/{id number}
+```
+
+### Testing POST
+Can only be used at
+```sh
+http://localhost:8080/list/
+```
+example of body:
+```sh
+    {
+        "id": "4",
+        "middleini": "X",
+        "lastname": "Dela Cruz",
+        "address": "Pampanga",
+        "mobile": "099999999",
+        "name": "Juan"
+        }
+```
+
+### Testing PUT
+Can only be used at
+```sh
+http://localhost:8080/list/{id number}
+```
+example of body:
+```sh
+    {
+        "id": "3",
+        "middleini": "Y",
+        "lastname": "Elcross",
+        "address": "Pampang",
+        "mobile": "091111111",
+        "name": "John"
+        }
+```
+
+### Testing DELETE
+Can only be used at:
+```sh
+http://localhost:8080/list/{id number}
+```
+
--- src/main/java/com/pleasee/basic/listo/list.java
+++ src/main/java/com/pleasee/basic/listo/list.java
@@ -6,23 +6,20 @@ public class list {
 	private String firstname;
 	private String middleini;
 	private String lastname;
-	private String mddress;
+	private String address;
 	private String mobile;
 	
-
-	
-
 public list() {
 	
 }
 	
-public list(String id, String firstname, String middleini, String lastname, String mddress, String mobile) {
+public list(String id, String firstname, String middleini, String lastname, String Address, String mobile) {
 	super();
 	this.id = id;
 	this.firstname = firstname;
 	this.middleini = middleini;
 	this.lastname = lastname;
-	this.mddress = mddress;
+	this.address = Address;
 	this.mobile = mobile;
 }
 	
@@ -38,26 +35,23 @@ public list(String id, String firstname, String middleini, String lastname, Stri
 	public void setName(String name) {
 		this.firstname = name;
 	}
-	public String getDescription() {
+	public String getMiddleini() {
 		return middleini;
 	}
-	public void setDescription(String Middlename) {
-		this.middleini = Middlename;
+	public void setMiddleini(String middleini) {
+		this.middleini = middleini;
 	}
 	public String getLastname() {
 		return lastname;
 	}
-
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
-
-	public String getMddress() {
-		return mddress;
+	public String getAddress() {
+		return address;
 	}
-
-	public void setMddress(String mddress) {
-		this.mddress = mddress;
+	public void setAddress(String Address) {
+		this.address = Address;
 	}
 
 	public String getMobile() {
